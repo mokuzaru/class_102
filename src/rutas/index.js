ec24ffd58e824f83ffc803cf692c90238c2da0a1
mokuzaru <kenny.otoya@pucp.pe>
2023-05-09 22:22:08 -0500

    put rutas

--- src/rutas/index.js
+++ src/rutas/index.js
@@ -13,7 +13,15 @@ rutas.post('/', (req,res) =>{
 rutas.post('/nombre', (req,res) => {
     console.log(req.body.fullname);
     console.log(req.body.age);
-    res.send('recibido x2');
+    let b = req.body.age;
+    let a = 18;
+    let c;
+    if(b < a){
+        c = "no ingreses a la web";
+    }else{
+        c = "bienvenido";
+    }
+    res.send(c);
 })
 
 module.exports = rutas;
